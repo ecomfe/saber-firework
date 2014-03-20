a331f5fd1229f292b9b75c31c8767af4a2d2973e
treelite <c.xinle@gmail.com>
2014-03-20 13:29:24 +0800

    start refactor

--- src/main.js
+++ src/main.js
@@ -5,22 +5,4 @@
 
 define(function (require) {
 
-    var View = require('./View');
-    var Model = require('./Model');
-    var Action = require('./Action');
-
-    return {
-        view: function (name) {
-            return new View(name);
-        },
-
-        model: function (name) {
-            return new Model(name);
-        },
-
-        action: function (name) {
-            return new Action(name);
-        }
-    };
-
 });
