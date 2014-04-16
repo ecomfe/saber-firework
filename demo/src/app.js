6cdae64c7b03eb9e597550f6140bf3d6aebaaf84
treelite <c.xinle@gmail.com>
2014-04-17 00:24:26 +0800

    support gesture

--- demo/src/app.js
+++ demo/src/app.js
@@ -6,6 +6,9 @@ define(function (require) {
     var Resolver = require('saber-promise');
     var firework = require('saber-firework');
 
+    // 添加手势事件支持
+    require('saber-firework/plugin/gesture');
+
     Resolver.disableExceptionCapture();
 
     firework.load({path: '/', action: require('./list')});
