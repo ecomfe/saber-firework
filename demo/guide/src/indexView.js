6b628279497bed186c90b8a736253126f6f375c9
treelite <c.xinle@gmail.com>
2014-04-21 16:30:49 +0800

    add guide demo

---
+++ demo/guide/src/indexView.js
@@ -0,0 +1,14 @@
+/**
+ * @file index view
+ */
+
+define(function () {
+
+    var config = {};
+
+    config.template = '<!-- target:main -->hello ${name}';
+
+    config.templateMainTarget = 'main';
+
+    return config;
+});
