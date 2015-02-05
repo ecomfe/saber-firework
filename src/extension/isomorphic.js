c1920a8aed3e6f164573f16aca4e398991950d8a
treelite <c.xinle@gmail.com>
2015-02-05 17:50:51 +0800

    Update extension for isomorphic

--- src/extension/isomorphic.js
+++ src/extension/isomorphic.js
@@ -1,5 +1,5 @@
 /**
- * @file Rebas扩展
+ * @file 同构扩展
  * @author treelite(c.xinle@gmail.com)
  */
 
@@ -7,15 +7,10 @@ define(function (require) {
     var globalConfig = require('../config');
 
     // 启动首评渲染
-    globalConfig.firstScreen = true;
-    // 设置首屏初始化变量名
-    globalConfig.initialDataKey = 'rebas';
+    globalConfig.isomorphic = true;
 
     // 设置路由器
     var router = require('saber-router');
     router.controller(require('saber-router/controller/popstate'));
     globalConfig.router = router;
-
-    // 设置默认的Model基类
-    globalConfig.Model = require('./RebasModel');
 });
